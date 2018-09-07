2a7bdc352edd3fb837c187f3726a383f033194a5
AA <an.antonelou@gmail.com>
2018-09-07 11:27:37 +0300

    Sign up/log in leads to homepage instead of profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
 	    if @user.save
           log_in @user
 	      flash[:success] = "Welcome to net-work!"
-      	  redirect_to @user
+      	  redirect_to root_path
 	    else
 	      render 'new'
 	    end
