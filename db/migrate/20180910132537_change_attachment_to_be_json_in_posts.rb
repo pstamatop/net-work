d78728ce2800cfc7b84634556208424e32826b36
AA <an.antonelou@gmail.com>
2018-09-14 14:37:46 +0300

    Check Heroku Error

--- db/migrate/20180910132537_change_attachment_to_be_json_in_posts.rb
+++ db/migrate/20180910132537_change_attachment_to_be_json_in_posts.rb
@@ -1,5 +1,5 @@
 class ChangeAttachmentToBeJsonInPosts < ActiveRecord::Migration[5.1]
   def change
-  	change_column :posts, :attachment, :json, using 'attachment::JSON'
+  	change_column :posts, :attachment, :json, using: 'attachment::JSON'
   end
 end
